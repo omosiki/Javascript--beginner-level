8f50baee4a814bae100664b2ae73794872b3f763
omosiki <abdullateefsikirat6@gmail.com>
2026-01-14 23:09:06 +0100

    Still working on the form validation

--- dom.js
+++ dom.js
@@ -43,3 +43,23 @@ function onSubmit(e){
         emailIput.value = ''
     }
 }
+
+Dom maniplation
+const ul = document.querySelector('.items');
+ul.remove()
+ul.lastElementChild.remove()
+ul.firstElementChild.textContent = 'jolly'
+ul.children[1].innerText ='siki';
+ ul.lastElementChild.innerHTML = '<h2>Bye</h2>'
+// // style
+const btn = document.querySelector('.btn');
+
+btn.style.background = 'red';
+ even lister
+ btn.addEventListener('moseover', (e) => {
+     e.preventDefault();
+     // console.log(e.target)
+   document.querySelector('#my-form').style.background = '#444'
+     document.querySelector('body').classList.add('#000')
+     document.querySelector('items').lastElementChild.innerHTML = '<h2>hello</h2>'
+ })
