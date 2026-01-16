687cfee8d977c3893616f0385f737a9c8ce5c90e
omosiki <abdullateefsikirat6@gmail.com>
2026-01-16 21:44:15 +0100

    Having issue setting a condition to the form

--- form.js
+++ form.js
@@ -3,21 +3,26 @@ const container = document.getElementById('container')
  const age = document.getElementById('age')
 const btn = document.getElementById('btn');
 
-
+  // create aform data
+      // Create a FormData object from the for
+      const formData = new formData(myform)
+      const ages = formData.get('age')
+      console.log(ages)
                     
 
 const x = 18
-    const m = 20
-btn.addEventListener('click', function(e){
+const m = 20
+myform.addEventListener('submit', function(e){
     e.preventDefault()
 
-    
+  
 if  (age < x ){
     console.log('still a child')
 }else if (age > m ){
     console.log('Welcome')
 }else{
-    // console.log('error')
+    console.log('error')
 }   
     
 })
+
